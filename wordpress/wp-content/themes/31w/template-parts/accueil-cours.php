252cad2345b4421c9e0eb52c98fd72f65b9257b9
Raritetnik <mkodarenko@gmail.com>
2022-12-02 12:57:40 -0500

    c26 Ajustement de l'affichage de la page accueil

--- wordpress/wp-content/themes/31w/template-parts/accueil-cours.php
+++ wordpress/wp-content/themes/31w/template-parts/accueil-cours.php
@@ -1,5 +1,13 @@
+<?php
+    $titre = get_the_title();
+    $code_cours = substr($titre,0,7);
+    $heure_cours = substr($titre,strrpos($titre,'('));
+    $titre = substr($titre,8);
+    $longueur = strlen($titre);
+?>
+
 <article class="grille__article">
-    <h6><?= get_the_title(); ?></h6>
+    <h6><?= $titre ?></h6>
     <?php $le_permalien = "<a href='" . get_the_permalink() . "'>Suite</a>"; ?>
     <p><?= wp_trim_words(get_the_excerpt(), 20,$le_permalien) ; ?></p>
     <p>Type de cours: <?php the_field("type_cours")?></p>
