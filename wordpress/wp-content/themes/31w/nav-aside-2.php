2e5d6499a07bba00111879421f811abb6253e2ef
Raritetnik <mkodarenko@gmail.com>
2022-12-13 14:32:46 -0500

    c3 Création de template et page de recherche

---
+++ wordpress/wp-content/themes/31w/nav-aside-2.php
@@ -0,0 +1,3 @@
+<div id="sidebar-nav-2" class="sidebar">
+	<?php dynamic_sidebar( 'nav-aside-2' ); ?>
+</div>
---
+++ wordpress/wp-content/themes/31w/search.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package underscore
+ */
+?>
+<!-- h1 class="trace">front-page.php</h1 -->
+<?php get_header(); ?>
+    <main class="site__main">
+		<section class="grille"><?php
+            if ( have_posts() ) :
+                /* Start the Loop */
+                while ( have_posts() ) :
+                    the_post();
+                    $mes_categories = get_the_category();
+                    if(in_category('galerie')):
+                        get_template_part('template-parts/accueil-galerie', '');
+                    elseif(in_category('note')):
+                        get_template_part( 'template-parts/accueil-note', '' );
+                    elseif(in_category('cours')):
+                        get_template_part('template-parts/accueil-cours', '');
+                    endif;
+                endwhile;
+            endif;?>
+        </section>
+    </main>
+<?php get_footer(); ?>
+</html>
